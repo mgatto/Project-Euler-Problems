8414de6f388f5aa84f4af8a37bd31fe1c21964ce
Michael Gatto <mgatto@lisantra.com>
2012-12-31 10:23:06 -0700

    Add docs explanaing my choices for this solution.

--- Problem 1/problem_1-for-loop-summing.php
+++ Problem 1/problem_1-for-loop-summing.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * This solution reflects the spirit of the canonical solution contained in 
+ * Project Euler's PDF. The canonical solution uses a defined function, then 
+ * calls it thrice: once to sum factors of 3, once for factors of 5 and lastly 
+ * to sum factors of 15: F(3) + F(5) - F(15).
+ *
+ * This uses a test nested within a for loop. The test contains two triggers 
+ * and sums at once.
+ *
+ * Some posted solutions for PHP append each sum into an array and then add 
+ * them at the end. I think that takes more memory than addition+substitution 
+ * in place.
+ *
+ * This solution also append an EOL char so it leaves a clean console :-)
+ */
 
 /* must have an initialized variable so we can += below! */
 $sum = 0;
